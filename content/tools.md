3cc6a4f47c54c1326fee72c5b927de291edf51c9
Jonathan Gilligan <jonathan.gilligan@vanderbilt.edu>
2022-08-22 12:30:22 -0500

    Initial commit.

---
+++ content/tools.md
@@ -0,0 +1,53 @@
+---
+title: Tools
+---
+
+# Software Tools for the Course
+
+## NetLogo
+
+### Get Started!
+
+1. [Install {{<NETLOGO>}}](https://ccl.northwestern.edu/netlogo/download.shtml)
+2. [Read the {{<NETLOGO>}} User Manual](https://ccl.northwestern.edu/netlogo/docs/)
+3. [Explore tutorials at OpenABM](https://www.comses.net/resources/education/)
+
+### Explore Published Models
+
+1. [The OpenABM Library](https://www.comses.net/codebases/)
+2. [{{<NETLOGO>}} User Community Models](https://ccl.northwestern.edu/netlogo/models/community/index.cgi)
+3. [{{<NETLOGO>}} Modeling Commons](http://modelingcommons.org/)
+
+## Analyze Behaviorspace Tool
+
+The [Analyze Behaviorspace]({{<abs_tool_url>}}) tool can be reached online at 
+[{{<abs_tool_url>}}]({{<abs_tool_url>}}), or if you use {{<RSTATS>}}, you can install the tool 
+on your own computer by following the instructions at [{{<abs_package_url>}}]({{<abs_package_url>}}).
+
+## R for Statistical Analysis
+
+You do not need to use {{<RSTATS>}} for this course, but people who are
+familiar with {{<RSTATS>}} may find that it is a powerful tool for analyzing
+{{<NETLOGO>}} models, and there are tools for integrating {{<NETLOGO>}} with
+{{<RSTATS>}}.
+
+
+### Get Started!
+
+1. Install the latest versions of {{<R_DOWNLOAD_URL>}} and 
+   {{<RSTUDIO_DOWNLOAD_URL>}} (free desktop edition).
+2. Install the [Analyze Behaviorspace]({{<abs_package_url>}}) tool for {{<RSTATS>}} so you can run
+   it on your own computer.
+
+### R Tools & Features
+
+1. {{<RSTUDIO>}}: Full-featured environment for working with {{<RSTATS>}}
+2. [Tidyverse](https://www.tidyverse.org/): A collection of tools designed for
+   Data Science in {{<RSTATS>}}.
+3. [RMarkdown](https://rmarkdown.rstudio.com/): A tool for integrating data
+   analysis in {{<RSTATS>}}, creating graphs and tables, and writing reports 
+   and papers.
+4. [R for Data Science](https://r4ds.had.co.nz/): A free open-source book on
+   using {{<RSTATS>}} for data science.
+5. [RNetLogo](http://rnetlogo.r-forge.r-project.org/): An {{<RSTATS>}} 
+   package for running {{<NETLOGO>}} models from within {{<RSTATS>}}.
---
+++ planning/regenerate_site.R
@@ -0,0 +1,105 @@
+library(rprojroot)
+library(tidyverse)
+pacman::p_load_gh("jonathan-g/blogdownDigest")
+pacman::p_load_gh("jonathan-g/semestr")
+# library(git2r)
+
+regenerate_site <- function(root = NULL, force = FALSE, keep_tex = FALSE) {
+  if (is.null(root)) {
+    root = find_root(criterion = has_file(".semestr.here"))
+  }
+  oldwd = setwd(root)
+  on.exit(setwd(oldwd))
+  message("Setting working directory to ", getwd())
+  semester <- load_semester_db("planning/EES-3310-5310.sqlite3")
+  generate_assignments(semester)
+  new_update_site(root = getwd(), force = force, keep_tex = keep_tex)
+}
+
+new_update_site <- function(root = NULL, force = FALSE, keep_tex = FALSE) {
+  if (is.null(root)) {
+    root = find_root(criterion = has_file(".semestr.here"))
+  }
+  oldwd = setwd(root)
+  on.exit(setwd(oldwd))
+  message("Setting working directory to ", getwd())
+  update_site(force = force)
+  out_opts = list(md_extensions = semestr:::get_md_extensions(), toc = TRUE,
+                  includes = list(in_header = "ees3310.sty"),
+                  keep_tex = keep_tex)
+  update_pdfs(force_dest = TRUE, force = force, output_options = out_opts)
+}
+
+init_git_tokens <- function(keyring = "git_access") {
+  if (keyring::keyring_is_locked(keyring)) {
+    try(keyring::keyring_unlock(keyring), silent = TRUE)
+    if (keyring::keyring_is_locked(keyring)) {
+      warning("Could not unlock keyring.")
+      return(invisible(NULL))
+    }
+  }
+  keys <- keyring::key_list(keyring = keyring)
+  gh_pat <- keys %>% filter(service == "GITHUB_PAT")
+  gl_pat <- keys %>% filter(service == "GITLAB_PAT")
+  Sys.setenv(GITHUB_PAT = keyring::key_get(gh_pat$service,
+                                           username = gh_pat$username,
+                                           keyring = keyring))
+  Sys.setenv(GITLAB_PAT = keyring::key_get(gl_pat$service,
+                                           username = gl_pat$username,
+                                           keyring = keyring))
+}
+
+config_cred <- function(val, lbl, repo) {
+  if(val) {
+    url <- git2r::remote_url(repo, lbl)
+    key_path <- NULL
+    # message("url = ", url)
+    if (str_starts(url, fixed("git@github.com"))) {
+      key_path <- git2r::ssh_path(file.path("github.com",
+                                            "id_ed25519_gh"))
+    } else if (str_starts(url, fixed("git@gitlab.com"))) {
+      key_path <- git2r::ssh_path(file.path("gitlab.com",
+                                            "id_ed25519_gl_com"))
+    } else if (str_starts(url, fixed("git@gitlab.jgilligan.org"))) {
+      key_path <- git2r::ssh_path(file.path("jg_gitlab", "id_ed25519"))
+    }
+    if (! is.null(key_path)) {
+      # message("key_path = ", key_path)
+      git2r::cred_ssh_key(
+        publickey = str_c(key_path, ".pub"),
+        privatekey = key_path,
+        passphrase = keyring::key_get("SSH_KEY_PASSWORD",
+                                      keyring = "git_access",
+                                      username = "jonathan")
+      )
+    } else {
+      NULL
+    }
+  } else {
+    git2r::cred_token(ifelse(lbl == "origin", "GITLAB_PAT",
+                             "GITHUB_PAT"))
+  }
+}
+
+publish <- function(ssh = NULL, repo = ".") {
+  init_git_tokens()
+  repo = git2r::repository(repo)
+
+  if (is.null(ssh)) {
+    remotes <- c("origin", "publish")
+    pattern <- "^git@([a-zA-Z][a-zA-Z0-9_\\-.]+):"
+    ssh <- map_lgl(remotes, ~str_detect(git2r::remote_url(repo, .x),
+                                        pattern)) %>%
+      set_names(remotes)
+  }
+
+  if (length(ssh) < 2) {
+    ssh <- rep_len(ssh, 2)
+  }
+
+  cred <- imap(ssh, config_cred, repo = repo)
+  git2r::push(".", name = "publish", refspec = "refs/heads/main",
+              credentials = cred$publish)
+  git2r::push(".", name = "origin", refspec = "refs/heads/main",
+              credentials = cred$origin)
+}
